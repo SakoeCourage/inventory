2372fd988a51a8fd26a017d046d74c748821c246
sakoe courage <akorlicourage@gmail.com>
2023-05-09 14:37:53 +0000

    before heroku

--- resources/js/api/Api.js
+++ resources/js/api/Api.js
@@ -7,6 +7,7 @@ let baseURL =
 
 
 let token = cookie.get('XSRF-TOKEN')
+console.log(token)
 let Api = axios.create({
     baseURL: `${baseURL}/api/v1`,
     'Authorization': `${token}`,
@@ -16,11 +17,9 @@ let Api = axios.create({
 Api.interceptors.response.use(function (response) {
     return response
 }, function (error) {
-
+    console.log(err)
     if (error?.response?.status === 401 || error?.response?.status === 419) {
         window.store.dispatch(clearCredentials())
-        console.log(err.response.status)
-
     } else if (error?.response?.status === 403) {
         alert('you don\'t enough priviledges')
     } else {
